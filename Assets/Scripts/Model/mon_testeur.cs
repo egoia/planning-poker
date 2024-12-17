33cbf3b1201c9ce55eeabe41e4868a1070bdedc3
bouloufa <melissabouloufa@gmail.com>
2024-12-18 00:04:10 +0100

    debug tests

--- Assets/Scripts/Model/mon_testeur.cs
+++ Assets/Scripts/Model/mon_testeur.cs
@@ -30,6 +30,41 @@ public class mon_testeur : MonoBehaviour
         File.WriteAllText(testFilePath, jsonData);
 
         /*
+        Card[] cartes = {Card.cinq, Card.cinq, Card.cinq};
+        Card res = Validator.strict(cartes);
+        if(Card.cinq!= res) Debug.Log("Validator.strict devrait retourner la carte commune");
+        else Debug.Log("test passé bien 1");
+
+        Card[] cartes = { Card.cinq, Card.deux, Card.cinq};
+        Card res = Validator.strict(cartes);
+        if(res!=null) Debug.Log("Validator.strict devrait retourner null s'il y a une carte différente des autres");
+        else Debug.Log("test passé bien 2");
+
+        Card[] cartes = {Card.trois, Card.cinq, Card.treize, Card.trois, Card.joker};
+        Card res = Validator.moyenne(cartes);
+        if(Card.cinq!= res) Debug.Log("Validator.moyenne ne calcule pas correctement la moyenne");
+        else Debug.Log("test passé bien 3");
+
+        Card[] cartes = {Card.trois, Card.cinq, Card.treize, Card.trois, Card.joker};
+        Card res = Validator.mediane(cartes);
+        if(Card.cinq.toInt()!= res.toInt()) Debug.Log("Validator.mediane ne calcule pas correctement la mediane");
+        else Debug.Log("test passé bien 4");
+
+        Card[] cartes = {Card.trois, Card.cinq, Card.trois, Card.treize, Card.trois, Card.trois, Card.joker};
+        Card res = Validator.majorite_absolue(cartes);
+        if(Card.trois!= res) Debug.Log("Validator.majorite_absolue ne calcule pas correctement la majorité absolue");
+        else Debug.Log("test passé bien 5");
+
+        Card[] cartes = {Card.huit, Card.cinq, Card.huit, Card.trois, Card.cafe};
+        Card res = Validator.majorite_relative(cartes);
+        if(Card.huit!= res) Debug.Log("Validator.majorite_relative ne calcule pas correctement la majorité relative");
+        else Debug.Log("test passé bien 6");
+
+        Card[] cartes = {Card.huit, Card.cinq, Card.huit, Card.cinq, Card.cafe};
+        Card res = Validator.majorite_relative(cartes);
+        if(res!=null) Debug.Log("Validator.majorite_relative devrait renvoyer null s'il n'y a pas de majorité");
+        else Debug.Log("test passé bien 7");
+        /*
         if(!File.Exists(testFilePath)) Debug.Log("Le fichier de test n'a pas été créé correctement.");
         string content = File.ReadAllText(testFilePath);
         Debug.Log(content);
@@ -65,15 +100,24 @@ public class mon_testeur : MonoBehaviour
         else Debug.Log("test passé");
         */
 
-        
+        /*
         Debug.Log("TEST test_NumberToCard()");
         Card res = Card.numberToCard(8);
-        Assert.AreEqual(Card.huit, res, "Card.numberToCard() ne retourne pas la bonne carte");
+        if(Card.huit!=res) Debug.Log("Card.numberToCard() ne retourne pas la bonne carte");
         
         Debug.Log("TEST test_NumberToCard_valeur_approximative()");
         int value = 15;
-        Card res = Card.numberToCard(value);
-        Assert.AreEqual(Card.treize, res, "Card.numberToCard() ne retourne pas la carte à la valeur la plus proche");
+        res = Card.numberToCard(value);
+        if(Card.treize!=res) Debug.Log("Card.numberToCard() ne retourne pas la carte à la valeur la plus proche");
+        */
+
+        var appManager = new AppManager(testFilePath, Validator.moyenne);
+        Card[] cartes = { Card.huit };
+        appManager.joue_tour(cartes);
+        appManager.save();
+        Assert.IsTrue(File.Exists(testFilePath), "Le fichier n'a pas été créé après la sauvegarde.");
+        var reloadedManager = new AppManager(testFilePath, Validator.moyenne);
+        Assert.AreEqual("Fonctionnalité 2", reloadedManager.getCurrent().getNom(), "Le gestionnaire ne devrait pas perdre sa progression après un sauvegarde/chargement.");
         
     }
 
