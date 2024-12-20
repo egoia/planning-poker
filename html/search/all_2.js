2e756f473a51b631d916cbfb5aaafcf2bedd1bb1
egoia <egoia@users.noreply.github.com>
2024-12-20 16:17:33 +0000

    deploy: 15e316190e2ada35e5dddae3ec6a5c3a0fa7c6b8

--- html/search/all_2.js
+++ html/search/all_2.js
@@ -5,7 +5,7 @@ var searchData=
   ['canvassampleopenfilejson_2',['CanvasSampleOpenFileJson',['../classCanvasSampleOpenFileJson.html',1,'']]],
   ['card_3',['card',['../classCard.html',1,'Card'],['../classPlanningPoker_1_1Card.html',1,'PlanningPoker.Card'],['../classCardSlot.html#a1d7c85a7ef9ce287f678730138a27f75',1,'CardSlot.card']]],
   ['cards_4',['cards',['../classDebateController.html#a60110827acf35318c845e939364353ce',1,'DebateController.cards'],['../classResultController.html#afc3ab42b48844f197513304b8f7ec305',1,'ResultController.cards']]],
-  ['cardsize_5',['cardsize',['../classDebateController.html#aadd380d4d7a1120a806bb958d02c4588',1,'DebateController.cardSize'],['../classCardSlot.html#a9941e4748e4b48a14fce74740a809574',1,'CardSlot.cardSize'],['../classResultController.html#a05c6824f3d77cf8756720823d70d17ec',1,'ResultController.cardSize']]],
+  ['cardsize_5',['cardsize',['../classCardSlot.html#a9941e4748e4b48a14fce74740a809574',1,'CardSlot.cardSize'],['../classResultController.html#a05c6824f3d77cf8756720823d70d17ec',1,'ResultController.cardSize'],['../classDebateController.html#aadd380d4d7a1120a806bb958d02c4588',1,'DebateController.cardSize']]],
   ['cardslot_6',['cardslot',['../classCardSlot.html',1,'CardSlot'],['../classGameController.html#aa9bd690cbd8e1eb1c1f10d94215e514e',1,'GameController.cardSlot']]],
   ['cardvalue_7',['cardValue',['../classDragCard.html#a7bbed292b446e9a8564f88829b362678',1,'DragCard']]],
   ['cent_8',['cent',['../classPlanningPoker_1_1Card.html#af53fd4f3897aff1617465ee3159793c9',1,'PlanningPoker::Card']]],
@@ -13,5 +13,6 @@ var searchData=
   ['chrono_10',['Chrono',['../classChrono.html',1,'']]],
   ['chronometertext_11',['chronometerText',['../classChrono.html#aa835d140eeb4b0c81bd355bc0ffb7021',1,'Chrono']]],
   ['cinq_12',['cinq',['../classPlanningPoker_1_1Card.html#a15eda93793c7ab8a7d8a9299583d571f',1,'PlanningPoker::Card']]],
-  ['contentnoms_13',['contentNoms',['../classMenuController.html#a5ecbb0f445107ddc4d097e3c47fc3486',1,'MenuController']]]
+  ['contentfonc_13',['contentFonc',['../classGameController.html#a3e220e0bbcc906b1c2b78b963fb5dbdc',1,'GameController']]],
+  ['contentnoms_14',['contentNoms',['../classMenuController.html#a5ecbb0f445107ddc4d097e3c47fc3486',1,'MenuController']]]
 ];
--- html/search/variables_4.js
+++ html/search/variables_4.js
@@ -1,5 +1,7 @@
 var searchData=
 [
   ['file_0',['file',['../classMenuController.html#a00b6aef420afdb7262bba9f339e1823f',1,'MenuController']]],
-  ['fonc_1',['fonc',['../classDebateController.html#a648a21ed2ee866fbd45e75b9d78c1702',1,'DebateController.fonc'],['../classResultController.html#a786403a2942f90d71a8c3c777de4a154',1,'ResultController.fonc']]]
+  ['fonc_1',['fonc',['../classDebateController.html#a648a21ed2ee866fbd45e75b9d78c1702',1,'DebateController.fonc'],['../classResultController.html#a786403a2942f90d71a8c3c777de4a154',1,'ResultController.fonc']]],
+  ['foncprefab_2',['foncPrefab',['../classGameController.html#a9b91609c95498f450aee75672f149a7c',1,'GameController']]],
+  ['fonctionnalites_3',['fonctionnalites',['../classPlanningPoker_1_1AppManager.html#a8fd02e1730aae81f28bad8e9ab05d3cc',1,'PlanningPoker::AppManager']]]
 ];
